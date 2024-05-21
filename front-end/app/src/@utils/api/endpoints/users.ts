8a8a1d62331ef9c64f976e29a948769913be0872
Daniel Adam <danieladamce@gmail.com>
2024-05-20 22:20:03 -0700

    code clean up

--- front-end/app/src/@utils/api/endpoints/users.ts
+++ front-end/app/src/@utils/api/endpoints/users.ts
@@ -1,17 +1,14 @@
-import { EndpointsClass } from ".";
 import { BodylessEndpoint, bodylessEndpoint } from "../utils";
 
 
 class UserEndpoints {
     readonly baseUrl: string
     fetchAll: BodylessEndpoint<Array<any>>
-    // fetch: BodylessEndpoint<any, {number}>
 
     constructor(url: string) {
         // super(url);
         this.baseUrl = url;
         this.fetchAll = bodylessEndpoint(url);
-        // this.fetch = bodylessEndpoint(url);
     }
 }
 
