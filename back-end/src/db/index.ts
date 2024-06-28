ee3c8de9afcb8d75710b638b2ff1e529225c5735
Daniel Adam <danieladamce@gmail.com>
2024-06-27 23:01:58 -0700

    ssl pg option for RDS

--- back-end/src/db/index.ts
+++ back-end/src/db/index.ts
@@ -1,25 +1,3 @@
-// import postgres from "postgres";
-// import dotenv from "dotenv";
-
-// dotenv.config({path: "./src/.env"});
-
-// const host = "localhost";
-// const port: number = Number(process.env.PG_PORT);
-// const database = process.env.PG_DATABASE;
-// const user = process.env.PG_USER;
-// const password = process.env.PG_PASSWORD;
-// const url = `postgres://${user}:${password}@${host}:${port}/${database}`;
-
-// const sql = postgres(url)
-
-// process.on('exit', () => {
-//     console.log("ending");
-//     sql.end({timeout: 5})
-// });
-
-// export default sql;
-
-
 import { Pool } from 'pg';
 import { Kysely, PostgresDialect } from 'kysely';
 import dotenv from "dotenv";
@@ -41,6 +19,9 @@ const dialect = new PostgresDialect({
         user: user,
         password: password,
         port: port,
+        ssl: {
+            rejectUnauthorized: false
+        },
         max: 10,
     })
 })
