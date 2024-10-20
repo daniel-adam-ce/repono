3396ca2070e72f3f653c4525b7c811e0c64b8980
Daniel Adam <danieladamce@gmail.com>
2024-10-20 00:03:25 -0700

    suppressed console in e2e test

--- back-end/src/__tests__/e2e/user.test.ts
+++ back-end/src/__tests__/e2e/user.test.ts
@@ -4,21 +4,24 @@ import { db } from "../../db";
 
 const api = request(createApp())
 
+console.error = jest.fn();
+console.log = jest.fn();
+
 describe("GET /users", () => {
     it("returns status 200 and an array of users", async () => {
         await api.get("/api/v1/users").expect(200);
     })
 })
 
-// describe("GET /users/:id", () => {
-//     it("returns status 401 with no credentials", async () => {
-//         await api.get("/api/v1/users/1").expect(401);
-//     })
+describe("GET /users/:id", () => {
+    it("returns status 401 with no credentials", async () => {
+        await api.get("/api/v1/users/1").expect(401);
+    })
 
-//     // it("returns status 200 and a user given a valid id", async () => {
-//     //     await api.get("/api/v1/users/1").expect(200);
-//     // })
-// })
+    // it("returns status 200 and a user given a valid id", async () => {
+    //     await api.get("/api/v1/users/1").expect(200);
+    // })
+})
 
 afterAll(() => {
     db.destroy();
